cf51e7bc8cdb728aa8ec2902448b1adb153dd00c
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-03-22 05:52:44 +0600

    Book List

---
+++ book_data.py
@@ -0,0 +1,5 @@
+books = {
+    "The Great Gatsby": {"author": "F. Scott Fitzgerald", "genre": "Classic Fiction", "description": "A tale of love, wealth, and corruption in the Jazz Age."},
+    "Harry Potter and the Sorcerer's Stone": {"author": "J.K. Rowling", "genre": "Fantasy", "description": "The first book in the Harry Potter series."},
+    # Add more books here
+}
